e6137aef2858f4c4441644d08e73c4e2dd20115c
Gareth Maddock <9137502+gtm19@users.noreply.github.com>
2022-01-03 08:00:57 +0000

    sketch-04 done

--- sketches/sketch-04.js
+++ sketches/sketch-04.js
@@ -1,18 +1,37 @@
 import canvasSketch from "canvas-sketch";
-import { random } from "canvas-sketch-util";
+import { random, math } from "canvas-sketch-util";
+import { Pane } from "tweakpane";
 
 const settings = {
   animate: true,
   dimensions: [ 1080, 1080 ]
 };
 
+const params = {
+  // grid
+  cols: 10,
+  rows: 10,
+  scaleMin: 1,
+  scaleMax: 20,
+  // noise
+  freq: 0.001,
+  amp: 0.2,
+  // animatiom
+  frame: 0,
+  animate: true,
+  // style
+  lineCap: "butt",
+  background: "#FFFFFF",
+  lineColour: "#000000"
+};
+
 const sketch = () => {
   return ({ context, width, height, frame }) => {
-    context.fillStyle = 'white';
+    context.fillStyle = params.background;
     context.fillRect(0, 0, width, height);
 
-    const cols = 12;
-    const rows = 12;
+    const cols = params.cols;
+    const rows = params.rows;
     const numCells = cols * rows;
 
     const gridWidth  = width  * 0.8;
@@ -35,10 +54,12 @@ const sketch = () => {
       let x = col * cellWidth;
       let y = row * cellHeight;
 
-      const n = random.noise2D(x + frame * 5, y + frame * 5, 0.001);
-      const angle = 0.2 * n * Math.PI * 2
+      const f = params.animate ? frame : params.frame
 
-      const scale = (n + 1) / 2 * 24;
+      const n = random.noise3D(x, y, f * 10, params.freq);
+      const angle = params.amp * n * Math.PI * 2
+
+      const scale = math.mapRange(n, -1, 1, params.scaleMin, params.scaleMax);
 
       // include overall plot margin
       x += marginX;
@@ -60,6 +81,8 @@ const sketch = () => {
       context.rotate(angle);
 
       context.lineWidth = scale;
+      context.lineCap = params.lineCap;
+      context.strokeStyle = params.lineColour;
       context.beginPath();
       // put pointer half a width to left of centre
       context.moveTo(-0.5 * w, 0);
@@ -73,5 +96,45 @@ const sketch = () => {
 
   };
 };
+
+const createPane = () => {
+  const pane = new Pane();
+  
+  const folderGrid = pane.addFolder(
+    { title: "Grid" }
+  );  
+  folderGrid.addInput(params, "cols", { min: 2, max: 50, step: 1  });
+  folderGrid.addInput(params, "rows", { min: 2, max: 50, step: 1  });
+  
+  folderGrid.addInput(params, "scaleMin", { min: 1, max: 100 });
+  folderGrid.addInput(params, "scaleMax", { min: 1, max: 100 });
+
+  const folderNoise = pane.addFolder(
+    { title: "Noise", expanded: false  }
+  );
+  folderNoise.addInput(params, "freq", { min: -0.01, max: 0.01 });
+  folderNoise.addInput(params, "amp", { min: 0, max: 1 });
+
+  const folderAnimation = pane.addFolder(
+    { title: "Animation", expanded: false  }
+  );
+  folderAnimation.addInput(params, "animate");
+  folderAnimation.addInput(params, "frame", { min:0, max: 999, step: 1 });
+
+  const folderStyle = pane.addFolder(
+    { title: "Style", expanded: false }
+  )
+  folderStyle.addInput(params, "lineCap", {
+    options: {
+      butt: "butt",
+      round: "round",
+      square: "square"
+    }
+  });
+  folderStyle.addInput(params, "background", { picker: 'inline', expanded: true });
+  folderStyle.addInput(params, "lineColour", { picker: 'inline', expanded: true });
+};
+
+createPane();
 
 canvasSketch(sketch, settings);
