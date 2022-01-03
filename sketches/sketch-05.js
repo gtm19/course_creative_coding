f8880042b6fba9fc5fb2783141fc27f0acf0d34c
Gareth Maddock <9137502+gtm19@users.noreply.github.com>
2022-01-03 09:46:45 +0000

    sketch-05 snapshot

---
+++ sketches/sketch-05.js
@@ -0,0 +1,48 @@
+const canvasSketch = require('canvas-sketch');
+
+const settings = {
+  dimensions: [ 1080, 1080 ]
+};
+
+let text = "A";
+let fontSize = 1200;
+let fontFamily = "serif";
+
+const sketch = () => {
+  return ({ context, width, height }) => {
+    context.fillStyle = 'white';
+    context.fillRect(0, 0, width, height);
+
+
+    context.fillStyle = "black";
+    context.font = `${fontSize}px ${fontFamily}`;
+    context.textBaseline = "top";
+    // context.textAlign = "center";
+
+    const metrics = context.measureText(text);
+    console.log(metrics);
+
+    const mx = -1 * metrics.actualBoundingBoxLeft;
+    const my = -1 * metrics.actualBoundingBoxAscent;
+    const mw = metrics.actualBoundingBoxLeft + metrics.actualBoundingBoxRight;
+    const mh = metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent;
+
+    //        [-----------------] <- add on the margin around the glyph
+    const x = (width - mw)  * 0.5 - mx;
+    //                            [--] <- move left of glyph all the way to the left of the canvas
+    const y = (height - mh) * 0.5 - my;
+
+    context.save();
+    context.translate(x, y);
+
+    context.beginPath();
+    context.rect(mx, my, mw, mh);
+    context.stroke();
+
+    context.fillText(text, 0, 0);
+    context.restore();
+  };
+
+};
+
+canvasSketch(sketch, settings);
